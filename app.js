8e18e5347f91c34a6904ea3c0d7c18ec1e4bdd0b
Jason Watson <jwatson@newerahd.com>
2015-04-15 10:46:59 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var app = angular.module('flapperNews', ['ui.router ']);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+
+  function($scope, posts){
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        upvotes: 0,
+        link: $scope.link
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+  }
+])
+
+app.config([
+  '$state'
+])
+
